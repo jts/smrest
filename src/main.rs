2a46f9bce456a2488b21ac37b94d75517376a2b4
Jared Simpson <jared.simpson@gmail.com>
2022-07-08 16:19:20 -0400

    implement simulation of generative process

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,21 @@ use rust_htslib::bcf::{Reader as BcfReader, Read as BcfRead};
 use std::collections::{HashSet, HashMap};
 use clap::{App, SubCommand, Arg};
 use itertools::Itertools;
+use rand::prelude::*;
+use statrs::distribution::{Beta, Poisson, Binomial, Discrete, ContinuousCDF};
 
 fn main() {
     let matches = App::new("smrest")
         .version("0.1")
         .author("Jared Simpson <jared.simpson@oicr.on.ca>")
         .about("Toolkit for estimating somatic mutation rate from long reads")
-        .subcommand(SubCommand::with_name("sim")
-                .about("generate a bam file containing simulated mutations")
-                .arg(Arg::with_name("mutation-rate")
-                    .short('r')
-                    .long("mutation-rate")
+        .subcommand(SubCommand::with_name("sim-pileup")
+                .about("simulate pileup data")
+                .arg(Arg::with_name("genome")
+                    .short('g')
+                    .long("genome")
                     .takes_value(true)
-                    .help("the rate of mutations to generate, expressed in number of mutations per megabase"))
-                .arg(Arg::with_name("input-bam")
-                    .required(true)
-                    .index(1)
-                    .help("the input bam file to process")))
+                    .help("the reference genome")))
         .subcommand(SubCommand::with_name("extract")
                 .about("gather candidate somatic mutations from aligned reads")
                 .arg(Arg::with_name("genome")
@@ -55,6 +53,8 @@ fn main() {
         error_contexts(matches.value_of("input-bam").unwrap(),
                        matches.value_of("genome").unwrap(),
                        matches.value_of("vcf").unwrap());
+    } else if let Some(matches) = matches.subcommand_matches("sim-pileup") {
+        sim_pileup(matches.value_of("genome").unwrap());
     }
 }
 
@@ -399,3 +399,207 @@ fn error_contexts(input_bam: &str, reference_genome: &str, variants_vcf: &str) {
     }
 }
 
+fn mutate_haplotypes(haplotypes: &mut [ Vec<u8> ], rate: f64) -> () {
+    // Set up a generator to select random bases
+    let bases = [ 'A', 'C', 'G', 'T' ];
+    let mut rng = rand::thread_rng();
+
+    // generate variants
+    for hi in 0..2 {
+        let mut _var_count = 0;
+        for i in 0..haplotypes[hi].len() {
+            if rng.gen::<f64>() < rate {
+                let curr_base = haplotypes[hi][i];
+                let mut var_base = curr_base;
+                while var_base == curr_base {
+                    var_base = bases[ rng.gen_range(0..4) ] as u8;
+                }
+                haplotypes[hi][i] = var_base;
+                _var_count += 1;
+            }
+        }
+        //eprintln!("Generate {_var_count} variants for haplotype");
+    }
+}
+
+fn calculate_class_probabilities(alt_count: u64, ref_count: u64, error_rate: f64, purity: f64, ccf_dist: &Beta, mutation_rate: f64, heterozygosity: f64) -> [f64;3]
+{
+
+    let depth = ref_count + alt_count;
+    // P(somatic | data) = P(data | somatic) P(somatic) / sum_class P(data | class )
+    // P(data | ref) = Binom(alt_count, ref_count + alt_count, error_rate)
+    let p_data_ref = Binomial::new(error_rate, depth).unwrap().pmf(alt_count);
+
+    // P(data | het) = Binom(alt_count, ref_count + alt_count, 1 - error_rate)
+    let p_data_het = Binomial::new(1.0 - error_rate, depth).unwrap().pmf(alt_count);
+    
+    // P(data | somatic) = sum_c Binom(alt_count, ref_count + alt_count, purity * c * (1 - error_rate) + (1 - purity*c) * error_rate ) P(c)
+    let bins = 10;
+    let step = 1.0 / 10 as f64;
+    let mut p_data_somatic = 0.0;
+    for i in 0..bins {
+        let start = f64::from(i) * step;
+        let end = f64::from(i + 1) * step;
+        let c = (end + start) / 2.0;
+        let p_c = ccf_dist.cdf(end) - ccf_dist.cdf(start);
+
+        let p_read_from_mutated_haplotype = purity * c;
+        let t1 = p_read_from_mutated_haplotype * (1.0 - error_rate);
+        let t2 = (1.0 - p_read_from_mutated_haplotype) * error_rate;
+
+        let p_data_somatic_at_c = Binomial::new(t1 + t2, depth).unwrap().pmf(alt_count);
+        p_data_somatic += p_data_somatic_at_c * p_c;
+    }
+
+    // priors
+    let p_het = heterozygosity;
+    let p_somatic = mutation_rate;
+    let p_ref = 1.0 - p_het - p_somatic;
+
+    let sum = p_data_ref * p_ref + p_data_het * p_het + p_data_somatic * p_somatic;
+    
+    let p_ref_data = p_data_ref * p_ref / sum;
+    let p_het_data = p_data_het * p_het / sum;
+    let p_somatic_data = p_data_somatic * p_somatic / sum;
+
+    return [ p_ref_data, p_het_data, p_somatic_data ];
+}
+
+fn sim_pileup(reference_genome: &str)
+{
+    let summary = false;
+    let mutation_rate = 5.0 / 1000000.0; // per haplotype
+    let heterozygosity = 1.0 / 2000.0; // per haplotype
+    let ccf_dist = Beta::new(9.0, 1.0).unwrap();
+    let depth_dist = Poisson::new(50.0).unwrap();
+    let purity = 0.75;
+    let error_rate = 0.02;
+
+    let mut rng = rand::thread_rng();
+
+    // get genome sequence
+    let chromosome_name = String::from("chr20");
+    //let chromosome_length = 60000000;
+    let chromosome_length = 6000000;
+
+    let faidx = faidx::Reader::from_path(reference_genome).expect("Could not read reference genome:");
+    let mut chromosome_sequence = faidx.fetch_seq_string(chromosome_name.as_str(), 0, chromosome_length).unwrap();
+    chromosome_sequence.make_ascii_uppercase();
+    let chromosome_bytes = chromosome_sequence.as_bytes();
+    let mut germline_haplotypes = [ chromosome_bytes.to_vec(), chromosome_bytes.to_vec() ];
+    mutate_haplotypes(& mut germline_haplotypes, heterozygosity);
+    
+    let mut somatic_haplotypes = [ germline_haplotypes[0].clone(), germline_haplotypes[1].clone() ];
+    mutate_haplotypes(& mut somatic_haplotypes, mutation_rate);
+
+    // Finally, simulate some pileup data at each position on each haplotype
+    if !summary {
+        println!("position\thaplotype\tclass\tis_het\tis_somatic\tccs\tref_count\talt_count\thvaf\tp_ref\tp_het\tp_somatic");
+    }
+
+    let mut sum_ref = 0.0;
+    let mut sum_het = 0.0;
+    let mut sum_somatic = 0.0;
+    let mut num_somatic_true = 0;
+    let mut num_somatic_calls = 0;
+    let mut num_somatic_correct = 0;
+    let mut num_het_true = 0;
+
+    for j in 0..chromosome_length {
+
+        // depth at this position
+        let depth = depth_dist.sample(&mut rng) as u64;
+        let haplotype_dist = Binomial::new(0.5, depth).unwrap();
+        let h0_depth = haplotype_dist.sample(&mut rng) as u64;
+        let haplotype_depths = [ h0_depth, depth - h0_depth ];
+
+        for i in 0..2 {
+            let is_het = germline_haplotypes[i][j] != chromosome_bytes[j];
+            let is_somatic = somatic_haplotypes[i][j] != germline_haplotypes[i][j];
+            let ccf = match is_somatic {
+                true => ccf_dist.sample(&mut rng),
+                false => 0.0
+            };
+
+            let mut ref_count = 0u64;
+            let mut alt_count = 0u64;
+
+            for _r in 0..haplotype_depths[i] {
+                // determine base for this read
+                let prob_sampled_from_somatic = match is_somatic {
+                    true => purity * ccf,
+                    false => 0.0
+                };
+
+                let is_tumor_read = rng.gen::<f64>() < prob_sampled_from_somatic;
+                let read_base = match is_tumor_read {
+                    true => somatic_haplotypes[i][j],
+                    false => germline_haplotypes[i][j]
+                };
+
+                let mut ref_match = read_base == chromosome_bytes[j];
+                if rng.gen::<f64>() < error_rate {
+                    ref_match = ! ref_match;
+                }
+
+                // update counts
+                ref_count += ref_match as u64;
+                alt_count += !ref_match as u64;
+            }
+
+            let probs = calculate_class_probabilities(alt_count, ref_count, error_rate, purity, &ccf_dist, mutation_rate, heterozygosity);
+
+            let mut class = "REF";
+            if is_somatic { 
+                class = "SOMATIC"
+            } else if is_het {
+                class = "HET";   
+            }
+
+            let mut p_max = 0.0;
+            let mut idx_max = 0;
+            for i in 0..probs.len() {
+                if probs[i] > p_max {
+                    p_max = probs[i];
+                    idx_max = i;
+                }
+            }
+
+            // update summary stats
+            sum_ref += probs[0];
+            sum_het += probs[1];
+            sum_somatic += probs[2];
+
+            if idx_max == 2 && is_somatic {
+                num_somatic_correct += 1;   
+            }
+            
+            if idx_max == 2 {
+                num_somatic_calls += 1;
+            }
+
+            if is_somatic { 
+                num_somatic_true += 1;
+            }
+
+            if is_het { 
+                num_het_true += 1;
+            }
+
+            // output per-record calls, if wanted
+            let hvaf = alt_count as f64 / haplotype_depths[i] as f64; 
+            if !summary {
+                println!("{j}\t{i}\t{class}\t{}\t{}\t{ccf:.3}\t\
+                         {ref_count}\t{alt_count}\t{hvaf:.3}\t{:.3}\t{:.3}\t{:.3}",
+                         is_het as u8, is_somatic as u8, probs[0], probs[1], probs[2]);
+            }
+        }
+    }
+
+    if summary {
+        let sens = num_somatic_correct as f64 / num_somatic_true as f64;
+        let prec = num_somatic_correct as f64 / num_somatic_calls as f64;
+        println!("true_somatic_positions\ttrue_het_position\testimated_ref_bases\testimated_het_bases\testimated_mutated_bases\tsomatic_call_sensitivity\tsomatic_call_precision");
+        println!("{num_somatic_true}\t{num_het_true}\t{sum_ref:.1}\t{sum_het:.1}\t{sum_somatic:.1}\t{sens:.3}\t{prec:.3}");
+    }
+}
